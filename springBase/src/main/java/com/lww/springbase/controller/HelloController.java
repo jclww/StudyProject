02f8dddd04e579258ea01d82dbf5b026dcf280ba
Li WeiWei <liweiwei6@btte.net>
2017-10-24 10:01:53 +0800

    完成基本环境

--- springBase/src/main/java/com/lww/springbase/controller/HelloController.java
+++ springBase/src/main/java/com/lww/springbase/controller/HelloController.java
@@ -1,10 +1,28 @@
 package com.lww.springbase.controller;
 
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  *
  * @author Lww
  * @date 2017/10/23
  */
+@RestController
 public class HelloController {
-
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @RequestMapping("/selectByName")
+    @ResponseBody
+    public String selectByName(ModelMap model, HttpServletRequest hsr) {
+        logger.info("asdasdas");
+        String str = "sadsa";
+        return JSON.toJSONString(str);
+    }
 }
