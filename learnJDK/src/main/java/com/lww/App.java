bf1de05558ed5542c45a6456f5683ca5169e836d
liweiwei <liweiwei@youzan.com>
2018-05-15 10:22:58 +0800

    1

--- learnJDK/src/main/java/com/lww/App.java
+++ learnJDK/src/main/java/com/lww/App.java
@@ -1,11 +1,13 @@
 package com.lww;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 
+import java.lang.reflect.Array;
 import java.security.MessageDigest;
 import java.security.MessageDigestSpi;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * Hello world!
@@ -19,11 +21,29 @@ public class App
 //            String uuid = UUID.randomUUID().toString().replaceAll("-", "");
 //            System.out.println(uuid);
 //        }
+        List<String> list = new ArrayList<>();
+        list.add("ass");
+        list.add("qqq");
+        Set<String> roleSet = new HashSet<>(list);
+        System.out.println(JSON.toJSONString(roleSet));
+        System.out.println(JSON.toJSONString(list));
 
-        String pwd = "21312sadwqhjpokmjjbhjkamsnqhgduhjqikwmenwqheqkweqjwoik3131";
-        String md = Hashing.md5().hashString(pwd, Charsets.UTF_8).toString()/*.newHasher().putString(pwd, Charsets.UTF_8).hash().toString()*/;
-        System.out.println(md+"\t"+md.length());
 
+        List<String> list1 = Arrays.asList("sales_business_operation","kf_business_operation");
+        List<String> list2 = Arrays.asList("[\"sales_business_operation\",\"kf_business_operation\"]");
+        System.out.println(JSON.toJSONString(list1));
+        System.out.println(JSON.toJSONString(list2));
+
+
+//        String pwd = "21312sadwqhjpokmjjbhjkamsnqhgduhjqikwmenwqheqkweqjwoik3131";
+//        String md = Hashing.md5().hashString(pwd, Charsets.UTF_8).toString()/*.newHasher().putString(pwd, Charsets.UTF_8).hash().toString()*/;
+//        System.out.println(md+"\t"+md.length());
+        long time =  1523991123000L;
+        long time2 = 1523944727206L;
+        long time3 = 1523945294000L;
+        Date date = new Date(time);
+        System.out.println(date.toLocaleString());
+        System.out.println(System.currentTimeMillis());
 
     }
 }
--- learnJDK/src/main/java/com/lww/learnjdk/Lambda/LearnLambda.java
+++ learnJDK/src/main/java/com/lww/learnjdk/Lambda/LearnLambda.java
@@ -2,12 +2,10 @@ package com.lww.learnjdk.Lambda;
 
 import com.alibaba.fastjson.JSON;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -42,6 +40,39 @@ public class LearnLambda {
         map.put("1","1");
         map.put("2","1");
         map.put("3","1");
+
+        IntStream.range(1, 3).forEach(System.out::println);
+        IntStream.rangeClosed(1, 3).forEach(System.out::println);
+        Stream<List<Integer>> inputStream = Stream.of(
+                Arrays.asList(1),
+                Arrays.asList(7, 5),
+                Arrays.asList(9, 4, 0)
+        );
+        Stream<List<Integer>> inputStream2 = Stream.of(
+                Arrays.asList(1),
+                Arrays.asList(7, 5),
+                Arrays.asList(9, 4, 0)
+        );
+        inputStream
+                .flatMap((childList) -> childList.stream())
+                .sorted( (x,y)->{
+                    System.out.println("xy"+x+y);
+                    return y-x;})
+                .forEach(System.out::println);
+
+        inputStream2
+                .flatMap((childList) -> childList.stream())
+                .sorted(Comparator.comparing(Integer::shortValue))
+                .forEach(System.out::println);
+
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        List<Integer> integers1 = integers.stream().filter(i -> i>2).collect(Collectors.toList());;
+        System.out.println(JSON.toJSONString(integers1));
 
 //        Arrays.stream(names).collect(Collectors.toMap(name, name));
 //        Map<String, String> map2 = players.stream().collect(Collectors.toMap(Function.identity(),Function.identity().andThen()));
